f7faff0ddbbcae026d28afea9563d6d0c111a1bc
Esinrogunjo <daudakale@gmail.com>
2022-04-29 22:54:45 +0100

    feat: prevent duplicate user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,10 +1,22 @@
 const User = require("../model/User");
 
 const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (existingUser)
+    return res
+      .status(400)
+      .json({ message: "user already exists, login instead" });
   const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
+    name,
+    email,
+    password,
   });
 
   try {
