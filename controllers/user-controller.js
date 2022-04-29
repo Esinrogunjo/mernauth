bfb24c995470c1fc37b64a0d50723efa1be72391
Esinrogunjo <daudakale@gmail.com>
2022-04-29 23:09:13 +0100

    feat:login and has password

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
 const User = require("../model/User");
+const bcrypt = require("bcryptjs");
 
 const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -13,10 +14,11 @@ const signup = async (req, res, next) => {
     return res
       .status(400)
       .json({ message: "user already exists, login instead" });
+  const hashPassword = bcrypt.hashSync(password);
   const user = new User({
     name,
     email,
-    password,
+    password: hashPassword,
   });
 
   try {
@@ -28,4 +30,26 @@ const signup = async (req, res, next) => {
   return res.status(201).json({ message: user });
 };
 
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    console.log(error);
+    return new Error(error);
+  }
+  if (!existingUser) {
+    return res.status(400).json({ message: "user not found, please signup" });
+  }
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "invalid email or password" });
+  }
+  res.status(200).json({ message: "welcome back man", existingUser });
+};
+
 exports.signup = signup;
+
+exports.login = login;
